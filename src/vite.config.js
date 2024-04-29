0227e091a8fc766995dca7aa7c0f6ccb80c74478
butburg <fire.fm@hotmail.de>
2024-04-29 20:06:41 +0200

    init

---
+++ src/vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+
+export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+        watch: {
+            usePolling: true
+        },
+        port: 5173,
+        hmr: {
+            host: 'localhost',
+        },
+        strictPort: true,
+    },
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+    ],
+});
