d2742816ba8ac3e5d63be35eadb63d910df4c5d4
butburg <fire.fm@hotmail.de>
2024-04-30 22:08:59 +0200

    specific vite config and make docker longterm running

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,20 +1,32 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
-export default defineConfig({
-    server: {
-        host: '0.0.0.0',
-        hmr: {
-            host: 'localhost'
-        }
-    },
-    plugins: [
-        laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
-            refresh: true,
-        }),
-    ],
+
+
+export default defineConfig(({ mode }) => {
+
+    const env = loadEnv(mode, process.cwd(), '')
+    console.log()
+    // vite config
+    return defineConfig({
+        server: {
+            //origin: 'http://127.0.0.1:8080', // Defines the origin of the generated asset URLs during development.
+            host: '0.0.0.0', // Bind to all network interfaces inside the container
+            port: 5173,      // Specify the strict port to use for Vite
+            strictPort: true, // Enforce strict port usage
+            hmr: {
+                host: env.VITE_APP_NAME || 'localhost',
+                port: 5173,
+            }
+        },
+        plugins: [
+            laravel({
+                input: [
+                    'resources/css/app.css',
+                    'resources/js/app.js',
+                ],
+                refresh: true,
+            }),
+        ],
+    })
 });
