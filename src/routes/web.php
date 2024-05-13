e4247d67d75cc977a73660ab253b4944266d54a4
butburg <fire.fm@hotmail.de>
2024-05-13 13:35:55 +0200

    create image postsroutes, migration and controller file

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -13,9 +14,21 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    // Routes of
+    // Posts, Profile,
+    // accessible to authenticated users only
+
+    // Profiles
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+
+
+    // Posts. use a resource route because it contains exact routes we need for a typical CRUD application.
+    Route::resource('posts', PostController::class);
+    Route::get('/posts/{post}/published', [PostController::class, 'published'])->name('posts.published');
+    Route::get('/posts/{post}/draft', [PostController::class, 'draft'])->name('posts.draft');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
