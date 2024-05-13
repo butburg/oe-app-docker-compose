d81fe948bdcbe3b8870d01e7ca538dd638e34d63
butburg <fire.fm@hotmail.de>
2024-05-13 13:35:55 +0200

    create image postsroutes, migration and controller file

---
+++ src/database/migrations/2024_05_13_111721_create_posts_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        // Create the 'posts' table with specified schema
+        Schema::create('posts', function (Blueprint $table) {
+            // Primary key 'id' field auto-incremented
+            $table->id();
+
+            // 'title' column to store the image title
+            $table->string('title');
+
+            // 'info_file' column to store information about attached files (nullable)
+            $table->string('info_file')->nullable();
+
+            // to store, if the post should be visible and published in gallery or only for the uploader
+            $table->boolean('is_published')->default(true);
+
+            // to store, if the impage from post contains faces or other content, that sould only available to logged in users
+            $table->boolean('is_sensitive')->default(false);
+
+            // 'created_at' and 'updated_at' columns to store timestamps of creation and updates
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
