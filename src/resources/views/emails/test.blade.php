b9e9c0f00289bdf382ff5e26289dfcf848ebd489
butburg <fire.fm@hotmail.de>
2024-05-21 19:55:08 +0200

    addign test mail

---
+++ src/resources/views/emails/test.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>MailHog Test Email</title>
+</head>
+<body>
+    <h1>MailHog Test Email</h1>
+    <p>This is a test email to check if MailHog is working correctly.</p>
+</body>
+</html>
