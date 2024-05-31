99ca0cad175c68de39a79bb4d245ccc754bc267e
butburg <fire.fm@hotmail.de>
2024-05-31 16:45:04 +0200

    improve navigation and header titles

--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Gallery') }}
+            {{ __('Oh Gallery') }}
         </h2>
     </x-slot>
 
