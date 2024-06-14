e02485e8b01d28368829c759f4e98bce03111ea0
butburg <fire.fm@hotmail.de>
2024-06-14 16:26:53 +0200

    reformat the form flex

--- src/resources/views/components/gallery/form/form.blade.php
+++ src/resources/views/components/gallery/form/form.blade.php
@@ -3,7 +3,7 @@
 <form action="{{ $action }}" method="POST" id="{{ $formId }}"
 @if($method === 'DELETE')
         onsubmit="return confirm('Are you sure you want to delete this comment?');"
-    @endif>
+    @endif class="flex items-center w-full">
     @csrf
     @if ($method)
         @method($method)
