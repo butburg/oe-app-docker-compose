7f4b354ed9fa7a548d7c9e415e3cf930b3101bd6
butburg <fire.fm@hotmail.de>
2024-06-17 18:17:18 +0200

    image tiles in creation

--- src/resources/views/components/text-button.blade.php
+++ src/resources/views/components/text-button.blade.php
@@ -1,3 +1,13 @@
-<button form="{{ $formId }}"
+@props(['color' => 'blue', 'formId' => '', 'buttonType' => 'submit'])
+
+@if ($buttonType === 'editToggle')
+    <button @click="isEditing = !isEditing"
     class="p-2 text-{{ $color }}-500 hover:bg-{{ $color }}-100 rounded"
-    type="submit">{{ $slot }}</button>
+    type="button">
+    <span x-show="!isEditing">Edit</span>
+    <span x-show="isEditing">Cancel</span>
+    </button>
+@else
+    <button form="{{ $formId }}" class="p-2 text-{{ $color }}-500 hover:bg-{{ $color }}-100 rounded"
+        type="submit">{{ $slot }}</button>
+@endif
