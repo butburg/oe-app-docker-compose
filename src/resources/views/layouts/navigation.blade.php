abef03e234e29b82849ed8af72956b86b63c5c63
butburg <fire.fm@hotmail.de>
2024-06-28 14:07:57 +0200

    change max width layout and beautify files

--- src/resources/views/layouts/navigation.blade.php
+++ src/resources/views/layouts/navigation.blade.php
@@ -1,18 +1,18 @@
-<nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
+<nav class="border-b border-gray-100 bg-white" x-data="{ open: false }">
     <!-- Navigation Headbar -->
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="flex justify-between h-16">
+    <div class="mx-auto max-w-screen-2xl px-4 sm:px-6 lg:px-8">
+        <div class="flex h-16 justify-between">
             {{-- Left navigation side --}}
             <div class="flex">
                 <!-- Logo -->
-                <div class="shrink-0 flex items-center">
+                <div class="flex shrink-0 items-center">
                     <a href="{{ route('dashboard') }}">
                         <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
                     </a>
                 </div>
                 @auth
                     {{-- left nav links logged in --}}
-                    <div class="space-x-8 sm:-my-px mx-5 flex">
+                    <div class="mx-5 flex space-x-8 sm:-my-px">
                         <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                             {{ __('Oh Gallery') }}
                         </x-nav-link>
@@ -22,11 +22,10 @@
                     </div>
                 @else
                     {{-- left nav links logged out --}}
-                    <div class="space-x-8 sm:-my-px mx-5 flex">
+                    <div class="mx-5 flex space-x-8 sm:-my-px">
                         <x-nav-link :href="route('login')" :active="request()->routeIs('login')">
                             {{ __('Log in') }}
                         </x-nav-link>
-
 
                         <x-nav-link :href="route('register')" :active="request()->routeIs('register')">
                             {{ __('Register') }}
@@ -34,20 +33,20 @@
                     </div>
                 @endauth
             </div>
-            
+
             <!-- right navigation side: Dropdown -->
             @auth
                 <!-- Hamburger for small screen -->
-                <div class="sm:hidden flex items-center ms-6">
+                <div class="ms-6 flex items-center sm:hidden">
                     <x-dropdown align="right" width="48">
                         <x-slot name="trigger">
                             <button
-                                class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
+                                class="inline-flex items-center justify-center rounded-md p-2 text-gray-400 transition duration-150 ease-in-out hover:bg-gray-100 hover:text-gray-500 focus:bg-gray-100 focus:text-gray-500 focus:outline-none">
                                 <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                                    <path :class="{ 'hidden': open, 'inline-flex': !open }" class="inline-flex"
+                                    <path class="inline-flex" :class="{ 'hidden': open, 'inline-flex': !open }"
                                         stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                         d="M4 6h16M4 12h16M4 18h16" />
-                                    <path :class="{ 'hidden': !open, 'inline-flex': open }" class="hidden"
+                                    <path class="hidden" :class="{ 'hidden': !open, 'inline-flex': open }"
                                         stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                         d="M6 18L18 6M6 6l12 12" />
                                 </svg>
@@ -57,9 +56,9 @@
                         <x-slot name="content">
                             <div class="space-y-1">
                                 <div
-                                    class="border-b-2 border-gray-200 block w-full ps-3 pe-4 py-2 text-start text-base font-medium text-gray-600">
-                                    <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
-                                    <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
+                                    class="block w-full border-b-2 border-gray-200 py-2 pe-4 ps-3 text-start text-base font-medium text-gray-600">
+                                    <div class="text-base font-medium text-gray-800">{{ Auth::user()->name }}</div>
+                                    <div class="text-sm font-medium text-gray-500">{{ Auth::user()->email }}</div>
                                 </div>
                                 <x-responsive-nav-link :href="route('profile.edit')">
                                     {{ __('Profile') }}
@@ -79,15 +78,15 @@
                 </div>
 
                 {{-- user menu for desktop --}}
-                <div class="hidden sm:flex sm:items-center sm:ms-6">
+                <div class="hidden sm:ms-6 sm:flex sm:items-center">
                     <x-dropdown align="right" width="48">
                         <x-slot name="trigger">
                             <button
-                                class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
+                                class="inline-flex items-center rounded-md border border-transparent bg-white px-3 py-2 text-sm font-medium leading-4 text-gray-500 transition duration-150 ease-in-out hover:text-gray-700 focus:outline-none">
                                 <div>{{ Auth::user()->name }}</div>
 
                                 <div class="ms-1">
-                                    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
+                                    <svg class="h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg"
                                         viewBox="0 0 20 20">
                                         <path fill-rule="evenodd"
                                             d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
