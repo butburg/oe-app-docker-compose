5d77d34acc8a4dba8438f65be37a22ba6816565c
butburg <fire.fm@hotmail.de>
2024-07-05 15:38:22 +0200

    add minimzed color theme and default font

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -13,103 +13,19 @@ export default {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ['"Nanum Myeongjo"', ...defaultTheme.fontFamily.sans],
             },
             colors: {
-                'main-color': {
-                    DEFAULT: '#00D7FF',
-                    50: '#B8F4FF',
-                    100: '#A3F1FF',
-                    200: '#7AEAFF',
-                    300: '#52E4FF',
-                    400: '#29DDFF',
-                    500: '#00D7FF',
-                    600: '#00A8C7',
-                    700: '#00788F',
-                    800: '#004957',
-                    900: '#001A1F',
-                    950: '#000203'
-                },
-                'brown-red': { DEFAULT: '#FF7300' }, // complementary red FF7300
-                
-                
-                'clear-red': { DEFAULT: '#FF4700' },
-                'orange': { DEFAULT: '#FF9D00' },
-                'cappuccino': { DEFAULT: '#806740' },
-                'main-green': { DEFAULT: '#00FF98' },
-                'main-orange': { DEFAULT: '#FFB100' },
-                'main-red': { DEFAULT: '#FF7300' },
-                'main-accent': { DEFAULT: '#3999AA' },
-                'gpt-red-comp': { DEFAULT: '#FF0054' },
-                'gpt-green': { DEFAULT: '#00FFAB' },
-                'gpt-red': { DEFAULT: '#FF5E00' },
-
-                // Navigation section
-                'nav-bg': '#001A1F',          // Dark background for the nav
-                'nav-text': '#A3F1FF',        // Light text color for nav links
-                'nav-text-hover': '#B8F4FF',  // Lighter text on hover
-                'nav-underline-hover': '#00D7FF',  // Underline color on hover
-                'yellow-text': '#FFB100',
-                // Title bar
-                'title-bg': '#004957',        // Dark background for title bar
-                'title-text': '#00D7FF',      // Main color for title text
-                'title-btn-bg': '#00A8C7',    // Button background in title bar
-                'title-btn-bg-hover': '#00788F', // Button background on hover
-
-                // Main content
-                'body-bg': '#001A1F',         // Body background color
-                'content-bg': '#004957',      // Background color for content tiles
-                'content-text': '#A3F1FF',    // Text color for content
-                'image-border': 'transparent', // No border for images
-                'input-bg': '#001A1F',        // Input field background
-                'input-text': '#A3F1FF',      // Input field text color
-                'comment-bg': '#00303F',      // Background for comment bubbles
-                'comment-text': '#A3F1FF',    // Text color for comment bubbles
-
-                // Buttons
-                'btn-primary-bg': '#00FFAB',  // Primary button background
-                'btn-primary-bg-hover': '#00C784', // Primary button background on hover
-                'btn-primary-text': '#001A1F', // Primary button text color
-                'btn-secondary-bg': '#FF0054', // Secondary button background (e.g., abort)
-                'btn-secondary-bg-hover': '#C70041', // Secondary button background on hover
-                'btn-secondary-text': '#001A1F', // Secondary button text color
-
-                // Links
-                'link-text': '#A3F1FF',       // Text color for links
-                'link-text-hover': '#B8F4FF', // Text color on hover for links
-
-              /*   #C2C2C2
-                #D06B64
-                #F83A22
-                #FA573C
-                #AC725E
-                #FF7537
-                #FFAD46
-                #FAD165
-                #FBE983
-                #B3DC6C
-                #7BD148
-                #41D692
-                #16A765
-                #92E1C0
-                #9FE1E7
-                #00D7FF
-                #9FC5E8
-                #4986E7
-                #9B9CFF
-                #B99AFF
-                #A47AE2
-                #CD74E7
-                #F691B2
-                #CABDBF
-                #CCA6AC */
+                'c-text': 'hsl(var(--text))',
+                'c-background': 'hsl(var(--background))',
+                'c-primary': 'hsl(var(--primary))',
+                'c-secondary': 'hsl(var(--secondary))',
+                'c-accent': 'hsl(var(--accent))',
             },
+
         },
     },
 
     plugins: [forms],
+
 };
-/**
- * 
- * 
- */
