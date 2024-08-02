192faf845978ccbc3a9dd1d9cb2de0bb83a760dd
butburg <fire.fm@hotmail.de>
2024-08-02 13:46:14 +0200

    make usernames unique

--- src/app/Http/Requests/ProfileUpdateRequest.php
+++ src/app/Http/Requests/ProfileUpdateRequest.php
@@ -16,7 +16,7 @@ class ProfileUpdateRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => ['required', 'string', 'max:255'],
+            'name' => ['required', 'string', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
             'profile_image' => ['nullable|image|mimes:jpeg,png,jpg,gif,svg|max:4096'],
         ];
