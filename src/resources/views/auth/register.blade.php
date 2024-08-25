934dd0c1c1867c6b610cfbdf79a776d3af07d306
butburg <fire.fm@hotmail.de>
2024-08-25 21:28:07 +0200

    add show password symbol for register form

--- src/resources/views/auth/register.blade.php
+++ src/resources/views/auth/register.blade.php
@@ -19,17 +19,42 @@
                 <x-input-error class="mt-2" :messages="$errors->get('email')" />
             </div>
 
-            <!-- Password -->
-            <div class="mt-4">
-                <x-input-label for="password" :value="__('New Password')" />
+<!-- Password -->
+<div class="relative mt-4">
+    <x-input-label for="password" :value="__('New Password')" />
 
-                <x-text-input class="mt-1 block w-full" id="password" name="password" type="password" required
-                    autocomplete="new-password" />
+    <x-text-input class="mt-1 block w-full pr-10" id="password" name="password" type="password" required
+        autocomplete="new-password" />
 
-                <x-input-error class="mt-2" :messages="$errors->get('password')" />
-            </div>
+    <!-- Show/Hide Password Toggle -->
+    <button class="absolute bottom-0 right-0 h-10 w-7 text-c-accent" type="button"
+        onmousedown="showPassword()" onmouseup="hidePassword()" onmouseleave="hidePassword()"
+        ontouchstart="showPassword()" ontouchend="hidePassword()">
+        <svg class="bi bi-eye-fill" id="togglePasswordIcon" xmlns="http://www.w3.org/2000/svg"
+            width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
+            <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0" />
+            <path
+                d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8m8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7" />
+        </svg>
 
-            {{-- Removed for eady of usage
+    </button>
+
+    <x-input-error class="mt-2" :messages="$errors->get('password')" />
+</div>
+
+            <script>
+                function showPassword() {
+                    const passwordField = document.getElementById('password');
+                    passwordField.type = 'text';
+                }
+
+                function hidePassword() {
+                    const passwordField = document.getElementById('password');
+                    passwordField.type = 'password';
+                }
+            </script>
+
+            {{-- Removed for ready of usage
             <!-- Confirm Password -->
             <div class="mt-4">
                 <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
