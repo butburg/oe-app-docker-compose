f60b473302337d57afd334d6fa0b33c182a4d31f
butburg <fire.fm@hotmail.de>
2024-11-13 17:58:00 +0100

    improve seeder for factories, split into classes, merge into one file DatabaseSeeder (delete admin file)

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -2,11 +2,14 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-use App\Models\Comment;
-use App\Models\Post;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Post;
+use App\Models\Comment;
+use App\Models\Image;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,24 +18,81 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // Create specific users
-        $users = User::factory(5)->create();
+        // Call the AdminSeeder
+        $this->call(AdminSeeder::class);
+        $this->call(UserSeeder::class);
+        $this->call(PostSeeder::class);
+        $this->call(CommentSeeder::class);
+    }
+}
 
+class AdminSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        $admin = User::factory()->create([
+            'name' => 'Admin User',
+            'email' => 'admin@user.com',
+            'password' => bcrypt('admin'), // Hash the password using bcrypt
+            'usertype' => 'admin',
+            'description' => fake()->sentence(),
+            'email_verified_at' => now(),
+        ]);
+
+        // Create for Admin a profile image
+        Image::factory()->count(1)->forUser($admin)->create();
+    }
+}
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the user seeding process.
+     */
+    public function run(): void
+    {
+        // Create specific users
+        $users = User::factory(4)->create();
+    }
+}
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the post seeding process.
+     */
+    public function run(): void
+    {
+        $users = User::all();
         // Create posts for these users
         $users->each(function ($user) {
-            Post::factory()->count(3)->forUser($user)->create();
+            Post::factory()->count(6)->forUser($user)->withImage()->create();
         });
+    }
+}
 
-        // Create comments for these posts
+class CommentSeeder extends Seeder
+{
+    /**
+     * Run the comment seeding process.
+     */
+    public function run(): void
+    {
+        // Retrieve all posts and create comments
         $posts = Post::all();
+        $users = User::all();
+
         // Iterate over each user
-        $users->each(function ($user) use ($posts) {
+        $users->each(function (User $user) use ($posts) {
             // Randomly select 3 posts for the user to comment on
-            $randomPosts = $posts->random(3);
-            $randomPosts->each(function ($post) use ($user) {
+            $randomPosts = $posts->random(6);
+
+            $randomPosts->each(function (Post $post) use ($user) {
                 Comment::factory()->count(1)->forUser($user)->forPost($post)->create();
             });
         });
-
     }
 }
