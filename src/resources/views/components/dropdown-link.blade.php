6e6e553da0b5e921be7d3cc8a68e6893eafbcae7
butburg <fire.fm@hotmail.de>
2026-03-06 20:14:59 +0100

    font little bit thicker

--- src/resources/views/components/dropdown-link.blade.php
+++ src/resources/views/components/dropdown-link.blade.php
@@ -1,2 +1,2 @@
 <a
-    {{ $attributes->merge(['class' => 'block w-full px-4 py-2 text-start text-sm leading-5 text-c-background hover:bg-c-background/20 focus:outline-none focus:bg-c-background/20 transition duration-150 ease-in-out']) }}>{{ $slot }}</a>
+    {{ $attributes->merge(['class' => 'block w-full px-4 py-2 text-start text-sm font-semibold leading-5 text-c-background hover:bg-c-background/20 focus:outline-none focus:bg-c-background/20 transition duration-150 ease-in-out']) }}>{{ $slot }}</a>
--- src/resources/views/components/responsive-nav-link.blade.php
+++ src/resources/views/components/responsive-nav-link.blade.php
@@ -3,8 +3,8 @@
 @php
     $classes =
         $active ?? false
-            ? 'block w-full ps-3 pe-4 py-2 border-l-4 border-main-color text-start text-base font-medium text-c-background bg-main-color-50 focus:outline-none focus:text-c-background focus:bg-main-color-100 focus:border-main-color-700 transition duration-150 ease-in-out'
-            : 'block w-full ps-3 pe-4 py-2 border-l-4 border-transparent text-start text-base font-medium text-c-background hover:text-c-background hover:bg-gray-50 hover:border-gray-300 focus:outline-none focus:text-c-background focus:bg-gray-50 focus:border-gray-300 transition duration-150 ease-in-out';
+            ? 'block w-full ps-3 pe-4 py-2 border-l-4 border-main-color text-start text-base font-semibold text-c-background bg-main-color-50 focus:outline-none focus:text-c-background focus:bg-main-color-100 focus:border-main-color-700 transition duration-150 ease-in-out'
+            : 'block w-full ps-3 pe-4 py-2 border-l-4 border-transparent text-start text-base font-semibold text-c-background hover:text-c-background hover:bg-gray-50 hover:border-gray-300 focus:outline-none focus:text-c-background focus:bg-gray-50 focus:border-gray-300 transition duration-150 ease-in-out';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
