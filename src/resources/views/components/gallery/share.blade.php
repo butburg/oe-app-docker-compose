cc69a179c41128e257265f5e376b532efef72e5e
butburg <fire.fm@hotmail.de>
2026-03-06 19:56:40 +0100

    add title for share button

--- src/resources/views/components/gallery/share.blade.php
+++ src/resources/views/components/gallery/share.blade.php
@@ -1,17 +1,14 @@
 @php
-    $page = app('App\Http\Controllers\PostController')->getPageForPost(
-        $post,
-        config('app.posts_per_page'),
-    );
+    $page = app('App\Http\Controllers\PostController')->getPageForPost($post, config('app.posts_per_page'));
     $shareUrl = route('dashboard') . "?page={$page}#post-{$post->id}";
 @endphp
 
 <!-- SVG Button for Sharing -->
-<button class="share-button" data-share-url="{{ $shareUrl }}"
-    title="Share Link" aria-label="Share Post">
+<button class="share-button" data-share-url="{{ $shareUrl }}" data-share-title="{{ $post->title }}" title="Share Link"
+    aria-label="Share Post">
     <!-- SVG Content Here -->
-    <svg class="mx-1 -mb-0.5" xmlns="http://www.w3.org/2000/svg" width="12"
-        height="12" fill="currentColor" viewBox="0 0 16 16">
+    <svg class="mx-1 -mb-0.5" xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor"
+        viewBox="0 0 16 16">
         <path
             d="M11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.5 2.5 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5" />
     </svg>
@@ -24,11 +21,13 @@
             button.addEventListener('click', function() {
                 const shareUrl = this.getAttribute(
                     'data-share-url');
+                const shareTitle = this.getAttribute(
+                    'data-share-title');
 
                 // Check if the browser supports the Share API
                 if (navigator.share) {
                     navigator.share({
-                        title: 'Check out this post from {{ config('app.name') }}',
+                        title: `${shareTitle} | {{ config('app.name') }}`,
                         url: shareUrl,
                     }).then(() => {
                         console.log(
